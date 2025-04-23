7bac30f29db366e8b0f37380fb7f806d63d1ae27
Bartiam <leswar@yandex.ru>
2025-04-23 16:15:45 +0500

    Added new asset matress and created new bplueprints.

---
+++ Source/StealthShooterUE5/WorldObjects/SSDoor_Base.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SSDoor_Base.h"
+
+// Sets default values
+ASSDoor_Base::ASSDoor_Base()
+{
+
+}
+
+// Called when the game starts or when spawned
+void ASSDoor_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
