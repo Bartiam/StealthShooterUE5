77de4f641b16de923e5bdbc294a6a2f9a032c9ea
Bartiam <leswar@yandex.ru>
2025-06-05 16:14:54 +0500

    Created new C++ class.

---
+++ Source/StealthShooterUE5/UserInterface/SS_KeypadWidget_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UserInterface/SS_CardReaderWidget_Base.h"
+#include "SS_KeypadWidget_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEALTHSHOOTERUE5_API USS_KeypadWidget_Base : public USS_CardReaderWidget_Base
+{
+	GENERATED_BODY()
+	
+};
