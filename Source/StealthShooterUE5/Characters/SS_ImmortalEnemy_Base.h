faf797ae13d841b708099cad9f0d3df813a44234
Bartiam <leswar@yandex.ru>
2025-06-11 20:28:42 +0500

    I started creating an AI.

---
+++ Source/StealthShooterUE5/Characters/SS_ImmortalEnemy_Base.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SSCharacter_Base.h"
+#include "SS_ImmortalEnemy_Base.generated.h"
+
+
+
+UCLASS()
+class STEALTHSHOOTERUE5_API ASS_ImmortalEnemy_Base : public ASSCharacter_Base
+{
+	GENERATED_BODY()
+	
+};
