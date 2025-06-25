e5d1d5f571d7b6c02fbd63d4e3294a6825eef8ec
Bartiam <leswar@yandex.ru>
2025-06-25 12:10:59 +0500

    Updated.

---
+++ Source/StealthShooterUE5/WorldGeneration/SS_RoomGeneration_Base.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SS_RoomGeneration_Base.h"
+
+// Sets default values
+ASS_RoomGeneration_Base::ASS_RoomGeneration_Base()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASS_RoomGeneration_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
